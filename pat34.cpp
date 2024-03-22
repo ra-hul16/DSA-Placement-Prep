0769ef4071b7b39fb2c5de9d14fc1aa35602b70b
ra-hul16 <138222343+ra-hul16@users.noreply.github.com>
2024-03-22 20:35:47 +0530

    Add files via upload

---
+++ pat34.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<i+1;j++){
+            if(j==0||j==i+1-1){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+            
+        }
+        cout<<endl;
+    }
+
+return 0;
+
+}
